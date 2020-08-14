6777211b539e0288f8b73382cee18bcc315677c6
고영일 <ykoh728@gmail.com>
2020-08-14 16:31:09 +0900

    make Weather component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Loading from './Loading';
+import Weather from './Weather';
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
 import axios from 'axios';
@@ -12,9 +13,10 @@ export default class extends React.Component {
 
 	getWeather = async (latitude, longitude) => {
 		const { data } = await axios.get(
-			`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+			`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
 		);
 		console.log(data);
+		this.setState({ isLoading: false, temp: data.main.temp });
 	};
 
 	getLocation = async () => {
@@ -35,7 +37,7 @@ export default class extends React.Component {
 		this.getLocation();
 	}
 	render() {
-		const { isLoading } = this.state;
-		return isLoading ? <Loading /> : null;
+		const { isLoading, temp } = this.state;
+		return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
 	}
 }
